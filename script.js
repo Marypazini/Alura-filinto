9c3edec9aa552327a84f2aa43c2abcad2f6da375
Marypazini <00001138387745sp@al.educacao.sp.gov.br>
2025-09-14 18:16:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternacontraste = document.getElementById('alterna-contraste');
+    
+    
 
     let tamanhoAtualFonte = 1;
 
@@ -22,6 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+    
+        alternacontraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
 
     })
 })
